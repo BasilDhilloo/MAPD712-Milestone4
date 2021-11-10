beb18c51d3119aadd63b3fef07a14c190ed438d1
Ashish Musani <ashishmusani@ashishs-air.home>
2021-11-09 21:17:05 -0500

    Add patient screen ready

--- screens/PatientsScreen/AddPatientScreen.js
+++ screens/PatientsScreen/AddPatientScreen.js
@@ -1,24 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Text, View, SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 import Icon from 'react-native-vector-icons/Ionicons';
 import AntIcons from 'react-native-vector-icons/AntDesign';
-import { TextInput } from 'react-native-gesture-handler';
+import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 
+function RadioButton(props) {
+    return (
+        <View style={[{
+          height: 24,
+          width: 24,
+          borderRadius: 12,
+          borderWidth: 2,
+          borderColor: '#000',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }, props.style]}>
+          {
+            props.selected ?
+              <View style={{
+                height: 12,
+                width: 12,
+                borderRadius: 6,
+                backgroundColor: '#000',
+              }}/>
+              : null
+          }
+        </View>
+    );
+  }
+
+var mobileNumberCodes = [...Array(99).keys()].slice(1);
 
 const AddPatientScreen = () => {
+
+    const [genderSelected, setGenderSelected] = useState('male');
+    const [selectedCountryCode, setSelectedCountryCode] = useState(0);
+
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.screenTitle}>Add patient</Text>
-            <Icon style={styles.patientAvatar} name="person-circle" size={80} />
-            <AntIcons 
-                style={styles.addPatientAvatar}
-                name="pluscircle" 
-                size={30} />
-            <Text style={styles.inputLabel}>Full Name</Text>
-            <View style={styles.textInputContainer}>
-                <TextInput style={styles.inputLabel}/>
-            </View>
-            <Text style={styles.inputLabel}>Date of birth</Text>
+            <ScrollView>
+                <Text style={styles.screenTitle}>Add patient</Text>
+                <Icon style={styles.patientAvatar} name="person-circle" size={80} />
+                <AntIcons 
+                    style={styles.addPatientAvatar}
+                    name="pluscircle" 
+                    size={30} />
+                <Text style={styles.inputLabel}>Full Name</Text>
+                <View style={styles.textInputContainer}>
+                    <TextInput style={styles.inputLabel}/>
+                </View>
+                <Text style={styles.inputLabel}>Date of birth</Text>
+                <View style={styles.textInputContainer}>
+                    <TextInput style={styles.inputLabel}/>
+                </View>
+                <Text style={styles.inputLabel}>Gender</Text>
+                <View style={{flexDirection:"row"}}>
+                    <View style={{flex: 1, flexDirection:"row"}}>
+                        <RadioButton />
+                        <Text style={{textAlignVertical: 'center', marginLeft: 5}}>Male</Text>
+                    </View>
+                    <View style={{flex: 1, flexDirection:"row"}}>
+                        <RadioButton />
+                        <Text style={{textAlignVertical: 'center', marginLeft: 5}}>Female</Text>
+                    </View>
+                </View>
+                <View style={{flexDirection:"row", marginTop: 5, marginBottom: 10}}>
+                        <RadioButton />
+                        <Text style={{textAlignVertical: 'center', marginLeft: 5}}>Choose not to specify</Text>
+                </View>
+                <Text style={styles.inputLabel}>Email address</Text>
+                <View style={styles.textInputContainer}>
+                    <TextInput style={styles.inputLabel}/>
+                </View>
+                <Text style={styles.inputLabel}>Mobile Number</Text>
+                <View style={{flexDirection:"row"}}>
+                    <View style={{flex: 2, flexDirection:"column"}}>
+                        <Picker
+                            selectedValue={selectedCountryCode}
+                            onValueChange={(itemValue, itemIndex) =>
+                                setSelectedCountryCode(itemValue) }>
+                                {mobileNumberCodes.map((item) => {
+                                    var lab = "+" + item;
+                                    return <Picker.Item label={lab} value={item} key={item}/> 
+                                }
+                            )}
+                        </Picker>
+                    </View>
+                    <View style={{...styles.textInputContainer, flex: 5}}>
+                        <TextInput style={styles.inputLabel}/>
+                    </View>
+                </View>
+                <View style={styles.saveButton}>
+                <Button 
+                    title="   SAVE   "/>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
@@ -49,6 +126,11 @@ const styles = StyleSheet.create({
     textInputContainer: {
         borderWidth: 1,
         marginBottom: 10
+    },
+    saveButton: {
+        alignSelf: 'center',
+        marginTop:10,
+
     }
   });
 export default AddPatientScreen
